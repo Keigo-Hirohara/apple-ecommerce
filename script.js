dd81eb0865a6c7606b3b64d362db3273cd09d534
hirokei-camel <hirokei.camel@gmail.com>
2022-04-13 10:38:36 +0900

    キューブの操作機能を実装しました

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
+// キューブ
+let x = 0;
+let y = 20;
+let z = 0;
+let active = true;
+let interval;
+
+const cube = document.querySelector('.cube');
+
+document.querySelector('.top-x-control').addEventListener('click', () => {
+  cube.style.transform = `rotateX(${(x += 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector('.bottom-x-control').addEventListener('click', () => {
+  cube.style.transform = `rotateX(${(x -= 20)}deg) rotateY(${y}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector('.right-y-control').addEventListener('click', () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${(y += 20)}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector('.left-y-control').addEventListener('click', () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${(y -= 20)}deg) rotateZ(${z}deg)`;
+});
+
+document.querySelector('.top-z-control').addEventListener('click', () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z -= 20)}deg)`;
+});
+
+document.querySelector('.bottom-z-control').addEventListener('click', () => {
+  cube.style.transform = `rotateX(${x}deg) rotateY(${y}deg) rotateZ(${(z += 20)}deg)`;
+});
+
+const playPause = () => {
+  if (active) {
+    interval = setInterval(() => {
+      cube.style.transform = `rotateX(${x}deg) rotateY(${y++}deg) rotateZ(${z}deg)`;
+    }, 100);
+  } else {
+    clearInterval(interval);
+  }
+};
+
+// playPause();
+
+document.querySelector('.controls').addEventListener('mouseover', () => {
+  active = false;
+  playPause();
+});
+
+document.querySelector('.controls').addEventListener('mouseout', () => {
+  active = true;
+  playPause();
+});
+// キューブ終了
+
 // スライドショー
 const slideshowDivs = () => {
   for (let i = 1; i <= 5; i++) {
